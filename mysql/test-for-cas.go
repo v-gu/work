37bd83753f1f3b3ca8b9a238b091421b2f5f474c
Vincent Gu <LiSNAz@gmail.com>
2011-07-18 17:36:34 +0800

    tc

--- mysql/test-for-cas.go
+++ mysql/test-for-cas.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 import "os"
 import "rand"
+import "time"
+//import "strconv"
 import mysql "github.com/Philio/GoMySQL"
 
 
@@ -24,8 +26,12 @@ func newDBConn() *mysql.Client {
 	return client
 }
 
+/*
+ Query1, DML(INSERT) on role_info.
+ */
 func query1(cli *mysql.Client) {
-	loop_count := 100
+	query_name := "[query1]"
+	loop_count := 1000
 	type row struct {
 		// PK: (userid, roleid, server, date)
 		userid		int32
@@ -34,60 +40,140 @@ func query1(cli *mysql.Client) {
 		date		string
 		name		string
 	}
-	rowgen := make(chan *row)
-
-	go func() {
-		for i := 0; i < loop_count; i++ {
-			newrow := &row{}
-			newrow.userid = rand.Int31n(100000)
-			newrow.roleid = rand.Int31n(10000)
-			newrow.server = int8(rand.Intn(100))
-			newrow.date = "2011-06-26 10:20:24"
-			newrow.name = "testname"
-			
-			rowgen <- newrow
-		}
-	}()
 	
-	fmt.Println("query1 start...")
-	for {
-		newrow := <-rowgen
-		stmt, err := cli.Prepare("INSERT INTO role_info VALUES(?,?,?,?,?)")
+	fmt.Printf("%s started...\n", query_name)
+	starttime := time.Nanoseconds()
+	for i := 0; i < loop_count; i++ {
+		newrow := &row{}
+		newrow.userid = rand.Int31n(100000)
+		newrow.roleid = rand.Int31n(10000)
+		newrow.server = int8(rand.Intn(100))
+		newrow.date = "2011-06-26 10:20:24"
+		newrow.name = "testname"
+
+		cli.Start()
+		stmt, err := cli.Prepare("INSERT IGNORE INTO " +
+			"role_info VALUES(?,?,?,?,?)")
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, query_name + err.String())
 			os.Exit(1)
 		}
 		err = stmt.BindParams(newrow.userid, newrow.roleid, newrow.server,
 			newrow.date, newrow.name)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, query_name + err.String())
 			os.Exit(1)
 		}
 		err = stmt.Execute()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, query_name + err.String())
 			os.Exit(1)
 		}
+		cli.Rollback()
 	}
-	fmt.Println("query1 end.")
+	endtime := time.Nanoseconds()
+	fmt.Printf("%s ended. Averange query time: %d nanosecs.\n", query_name,
+		(endtime-starttime)/((int64)(loop_count)))
 }
 
+/*
+ Query2, DML(UPDATE) on role_info.
+ */
+func query2(cli *mysql.Client) {
+	query_name := "[query2]"
+	loop_count := 10
+
+	fmt.Printf("%s started...\n", query_name)
+	starttime := time.Nanoseconds()
+	for i := 0; i < loop_count; i++ {
+
+		cli.Start()
+		stmt, err := cli.Prepare("UPDATE role_info SET NAME = ? " +
+			"WHERE name IS NULL")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		err = stmt.BindParams("'testname'")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		err = stmt.Execute()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		cli.Rollback()
+	}
+	endtime := time.Nanoseconds()
+	fmt.Printf("%s ended. Averange query time: %d nanosecs.\n", query_name,
+		(endtime-starttime)/((int64)(loop_count)))
+}
+
+
+/*
+ Query3, DML(DELETE) on role_info.
+ */
+func query3(cli *mysql.Client) {
+	query_name := "[query3]"
+	loop_count := 1000
+
+	fmt.Printf("%s started...\n", query_name)
+	starttime := time.Nanoseconds()
+	for i := 0; i < loop_count; i++ {
+
+		cli.Start()
+		stmt, err := cli.Prepare("DELETE FROM role_info SET NAME = ? " +
+			"WHERE name IS NULL")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		err = stmt.BindParams("'testname'")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		err = stmt.Execute()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, query_name + err.String())
+			os.Exit(1)
+		}
+		cli.Rollback()
+	}
+	endtime := time.Nanoseconds()
+	fmt.Printf("%s ended. Averange query time: %d nanosecs.\n", query_name,
+		(endtime-starttime)/((int64)(loop_count)))
+}
+
+
 func main() {
-	fmt.Println("Test start...")
+	fmt.Println("Test suite started...")
+
+	fmt.Println("indivisual test:")
+	// query1(newDBConn())
+	// query2(newDBConn())
+	query3(newDBConn())
 	
-	queries := 1
+	queries := 2
 	workpipe := make(chan int)
 
-	// init query1
-	go func() {
-		query1(newDBConn())
-		workpipe <- 1
-	} ()
+	// // do query1
+	// go func() {
+	// 	query1(newDBConn())
+	// 	workpipe <- 1
+	// } ()
+
+	// // do query2
+	// go func() {
+	// 	query2(newDBConn())
+	// 	workpipe <- 1
+	// } ()
+
+	
 
 	for i := 0; i < queries; i += <-workpipe { }
-
-	// end
-	fmt.Println("Test end.")
 }
 
 func mysql_init() {
